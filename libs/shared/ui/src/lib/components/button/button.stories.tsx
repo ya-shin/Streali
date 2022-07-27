0cd37afaa7a2446625f363c613d9dce595d5fa4c
Wiliam Traoré <hi@wil-liam.com>
2022-07-27 11:38:50 +0200

    feat(button): update storybook

--- libs/shared/ui/src/lib/components/button/button.stories.tsx
+++ libs/shared/ui/src/lib/components/button/button.stories.tsx
@@ -1,9 +1,24 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button } from './button';
+import { Button, ButtonColor, ButtonSize } from './button';
 
 export default {
   component: Button,
   title: 'Button',
+  argTypes: {
+    color: {
+      options: [ButtonColor.Primary, ButtonColor.Dark, ButtonColor.Error],
+      control: { type: 'select' },
+    },
+    size: {
+      options: [
+        ButtonSize.Normal,
+        ButtonSize.Big,
+        ButtonSize.Small,
+        ButtonSize.Very_Small,
+      ],
+      control: { type: 'select' },
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
