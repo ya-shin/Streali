6d7a0e330dc4970d68320a971b3f937f9b4bef32
Jérémy Lopes <dev.jlopes@gmail.com>
2022-08-01 19:41:16 +0200

    chore(project): fix Tailwind setup

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -1,11 +1,4 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
-const { join } = require('path');
-
 module.exports = {
-  content: [
-    join(__dirname, 'libs/**/**/src/**/**/*!(*.stories|*.spec).{ts,tsx,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
   theme: {
     extend: {
       fontFamily: {
