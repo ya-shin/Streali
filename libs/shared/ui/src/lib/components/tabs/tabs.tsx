9b4c54105edad7233828f8dc155d36a4580d5aaa
Jérémy Lopes <dev.jlopes@gmail.com>
2022-08-04 22:57:02 +0200

    feat(tabs): add tabs component

--- libs/shared/ui/src/lib/components/tabs/tabs.tsx
+++ libs/shared/ui/src/lib/components/tabs/tabs.tsx
@@ -1,19 +1,34 @@
 import * as TabsLib from '@radix-ui/react-tabs';
-import Button, { ButtonSize } from '../button/button';
+import { useState } from 'react';
+import Button, { ButtonColor, ButtonSize } from '../button/button';
 
 export interface TabsProps {
-  content: { title: string; content: React.ReactNode; disabled?: boolean }[];
+  content: TabProps[];
+}
+
+export interface TabProps {
+  title: string;
+  content: React.ReactNode;
+  disabled?: boolean;
 }
 
 export function Tabs(props: TabsProps) {
   const { content } = props;
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleChange = (selectedIndex: number) => {
+    const selectedTab: TabProps = content[selectedIndex];
+    
+    if (!selectedTab.disabled)
+      setActiveTabIndex(selectedIndex);
+  };
 
   return (
     <TabsLib.Root defaultValue="tab-0">
       <TabsLib.List className="flex gap-2 mb-3">
         {content.map(({ title, disabled }, index) => (
           <TabsLib.Trigger value={'tab-' + index} disabled={disabled}>
-            <Button size={ButtonSize.Small} disabled={disabled}>
+            <Button color={activeTabIndex === index ? ButtonColor.Primary : ButtonColor.Dark} size={ButtonSize.Small} disabled={disabled} onClick={() => handleChange(index)}>
               {title}
             </Button>
           </TabsLib.Trigger>
