6b55d68471396ffd840a4203a25e9d2123fb6ef8
Wiliam Traoré <hi@wil-liam.com>
2022-08-26 10:56:33 +0200

    feat(login): add login w/ supabase

--- apps/front/pages/login/index.tsx
+++ apps/front/pages/login/index.tsx
@@ -1,16 +1,21 @@
 import { Button } from '@streali/shared/ui';
+import { supabase } from '@streali/shared/utils';
 
 /* eslint-disable-next-line */
 export interface LoginProps {}
 
 export function Login(props: LoginProps) {
+  console.log(supabase.auth.session());
+
+  const handleTwitchLogin = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'twitch',
+    });
+  };
+
   return (
-    <div>
-      <Button
-        iconLeft="twitch-fill"
-        link="http://localhost:1337/api/connect/twitch"
-        external
-      >
+    <div className="h-screen flex justify-center items-center">
+      <Button iconLeft="twitch-fill" onClick={handleTwitchLogin}>
         Login with Twitch
       </Button>
     </div>
