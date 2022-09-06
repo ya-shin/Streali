bfc8b62a2afeac630dadb0a34b4562c8d40ef48b
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-09-06 22:32:11 +0200

    feat(color-picker): add custom color-picker

---
+++ libs/shared/ui/src/lib/components/forms/color-picker/color-picker.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Saturation from '@uiw/react-color-saturation';
+import Hue from '@uiw/react-color-hue';
+import { HsvaColor } from '@uiw/color-convert';
+import { Tabs } from './tabs';
+import { Pointer } from './pointer';
+import { rgbaStringToHsva, color as colorResult } from '@uiw/color-convert';
+
+export interface ColorPickerProps {
+  color?: HsvaColor;
+  onChange: (value: string) => void;
+  className?: string;
+}
+
+
+export const ColorPicker = (props: ColorPickerProps) => {
+  const { color = rgbaStringToHsva('rgba(255,0,0,1)'), onChange, className } = props;
+  const [hsva, setHsva] = React.useState<HsvaColor>(color);
+
+  React.useEffect(() => {    
+    onChange(colorResult(hsva).hexa);
+  }, [hsva, onChange]); 
+
+  //TODO: fix pointer position
+
+  return (
+    <div className={`color-picker flex w-56 rounded-md bg-dark-500 border-dark-300 border-2 box-border  ${className}`}>
+      <Hue
+        hue={hsva.h}
+        onChange={(newHue) => {
+          setHsva({ ...hsva, ...newHue });
+        }}
+        pointer={Pointer}
+        radius="4px 0 0 4px"
+        direction="vertical"
+        width="6px"
+        height="320px"
+      />
+      <div className="flex flex-col gap-4" style={{ width: '214px' }}>
+        <Saturation
+          hsva={hsva}
+          onChange={(newColor) => setHsva({ ...hsva, ...newColor, a: hsva.a })}
+          radius="0 4px 0 0"
+          style={{ height: '240px', width: '100%' }}
+          pointer={Pointer}
+        />
+        <Tabs color={hsva} onChange={(newColor) => setHsva(newColor)} />
+      </div>
+    </div>
+  );
+};
+
+
