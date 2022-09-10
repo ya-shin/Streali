e0bcabb5fa1491e904a8581f626e2270d0dfb5e2
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-09-10 11:47:52 +0200

    feat(font-select): can now grab

--- libs/shared/ui/src/lib/components/forms/font-select/font-select.tsx
+++ libs/shared/ui/src/lib/components/forms/font-select/font-select.tsx
@@ -2,12 +2,17 @@ import { useGoogleFonts } from '@streali/shared/hooks';
 import { SingleValue } from 'react-select';
 import Select from '../select/select';
 
+export interface fontVariants {
+  label: string;
+  value: string;
+}
+
 export interface FontSelectProps {
   label?: string;
   className?: string;
   value?: string;
   defaultValue?: { value: string; label: string };
-  onChange?: (fontName: string, variants: string[]) => void;
+  onChange?: (fontName: string, variants: fontVariants[]) => void;
   disabled?: boolean;
 }
 
@@ -22,12 +27,43 @@ export function FontSelect(props: FontSelectProps) {
 
   const { data: fonts } = useGoogleFonts();
 
+  const convertFontWeight = (weight: string) => {
+    switch (weight) {
+      case '100':
+        return 'Thin';
+      case '200':
+        return 'Extra Light';
+      case '300':
+        return 'Light';
+      case '400':
+        return 'Regular';
+      case '500':
+        return 'Medium';
+      case '600':
+        return 'Semi Bold';
+      case '700':
+        return 'Bold';
+      case '800':
+        return 'Extra Bold';
+      case '900':
+        return 'Black';
+      default:
+        return weight;
+    }
+  };
+
   function handleChange(value: SingleValue<{ value: string; label: string }>) {
     const font = fonts?.find((f) => f.family === value?.value);
     const variants = font?.variants.filter((v) => !v.includes('italic'));
 
     if (font) {
-      onChange && onChange(font.family, variants ? variants : []);
+      onChange &&
+        onChange(
+          font.family,
+          variants
+            ? variants?.map((v) => ({ label: convertFontWeight(v), value: v }))
+            : []
+        );
     }
   }
 
